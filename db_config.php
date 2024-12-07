b3892f252eaa92c7a16aa01a02f2ca1d36f80aa9
Tausia01 <tausia.tahsin@gmail.com>
2024-12-08 00:59:55 +0600

    database config file

---
+++ db_config.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+$servername = "localhost";  // Database server (localhost if running locally)
+$username = "root";         // MySQL username (default for XAMPP is "root")
+$password = "";             // MySQL password (default for XAMPP is an empty string)
+$database = "ClassroomBooking";  // Name of your database
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
